17e1cc5f336b4704327f1e75aefa64d5a2bcc183
ridwaanhall <erbyl14@gmail.com>
2023-11-22 21:09:34 +0700

    adds text type

--- 2023/01_recap_of_2023.py
+++ 2023/01_recap_of_2023.py
@@ -1,29 +1,22 @@
 # %%
 import os
 import time
-from PIL import Image
+import matplotlib.pyplot as plt
+
+# %%
+# Get the absolute path to the image file
+script_dir = os.path.dirname(os.path.abspath(__file__))
+image_folder = os.path.join(script_dir, "images")
 
 # %%
 def type_with_total_time(text, total_time=5.3):
     delay = total_time / len(text)
-
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
 
-def display_image_with_delay(image_path, delay=0.3):
-    image = Image.open(image_path)
-    image.show()
-    time.sleep(delay)
-
-text_to_type = "and with that the 2023 season"
-total_typing_time = 3.5
+text_to_type = "and with that the 2023 season comes to an end, good night<3"
+total_typing_time = 5.3
 
 type_with_total_time(text_to_type, total_typing_time)
-
-image_folder = "2023/images"
-image_path1 = os.path.join(image_folder, "image1.jpeg")
-display_image_with_delay(image_path1)
-
-image_path2 = os.path.join(image_folder, "image2.jpeg")
-display_image_with_delay(image_path2)
+# %%
