625802a18c19d3ba4eea78b661bb4606042ef8bd
ridwaanhall <erbyl14@gmail.com>
2023-11-22 20:19:41 +0700

    make total of text is 5.3 second

--- 2023/01_recap_of_2023.py
+++ 2023/01_recap_of_2023.py
@@ -1,9 +1,15 @@
 import time
 
-def type_with_delay(text, delay=3.5):
+def type_with_total_time(text, total_time=3.5):
+    # Calculate the delay per character
+    delay = total_time / len(text)
+
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
 
-text_to_type = "and with that the 2023 season"
-type_with_delay(text_to_type)
+text_to_type = "and with that the 2023 season comes to an end, good night <3"
+total_typing_time = 5.3
+
+type_with_total_time(text_to_type, total_typing_time)
+
